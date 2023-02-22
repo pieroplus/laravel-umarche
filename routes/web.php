df6930ab0c4045d3fef47507c6b2fc71e9d0ef7e
pieroplus <hey_taxi_stop_please@yahoo.co.jp>
2023-02-23 01:01:13 +0900

    コンポーネントの準備

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ComponentTestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
+Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
