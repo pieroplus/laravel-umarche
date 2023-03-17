5b115915bb924227241fdb6d37841d3298340dc6
pieroplus <hey_taxi_stop_please@yahoo.co.jp>
2023-03-18 00:44:47 +0900

    product Store

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,7 +16,17 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = [
-
+        'shop_id',
+        'name',
+        'information',
+        'price',
+        'is_selling',
+        'sort_order',
+        'secondary_category_id',
+        'image1',
+        'image2',
+        'image3',
+        'image4',
     ];
 
     public function shop(): BelongsTo
--- resources/views/owner/products/index.blade.php
+++ resources/views/owner/products/index.blade.php
@@ -21,7 +21,7 @@
                     <div class="border rounded-md p-2 md:p-4">
                       <div class="mb-4">
                         <div>
-                          <x-thumbnail :filename="$product->imageFirst->filename" type="products" />
+                          <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
                           <div class="text-gray-700">
                             {{ $product->name }}
                           </div>
