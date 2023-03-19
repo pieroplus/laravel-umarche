41e647b76a0f91c9c26e50b8a296bfa25bdd570b
pieroplus <hey_taxi_stop_please@yahoo.co.jp>
2023-03-20 02:02:28 +0900

    viewの調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -11,20 +11,16 @@
         <div class="p-6 text-gray-900">
           <div class="flex flex-wrap">
             @foreach($products as $product)
-              <div class="w-1/4 p-2 md:p-4 mt-2">
-                {{-- <a href="{{ route('owner.products.edit', ['product' => $product->id]) }}"> --}}
-                  <div class="border rounded-md p-2 md:p-4">
-                    <div class="mb-4">
-                      <div>
-                        <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
-                        <div class="text-gray-700">
-                          {{ $product->name }}
-                        </div>
-                      </div>
-                    </div>
+            <div class="w-1/4 p-2 md:p-4">
+                <div class="border rounded-md p-2 md:p-4">
+                  <x-thumbnail filename="{{$product->imageFirst->filename ?? ''}}" type="products" />
+                  <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category->name }}</h3>
+                    <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
+                    <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></p>
                   </div>
-                </a>
-              </div>
+                </div>
+            </div>
             @endforeach
           </div>
         </div>
