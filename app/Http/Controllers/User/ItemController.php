a116dbcd332cfb2030680c09bc887ef37f3342b0
pieroplus <hey_taxi_stop_please@yahoo.co.jp>
2023-03-20 01:20:57 +0900

    user.items.view調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function index(): View
     {
-        return view('user.index');
+        $products = Product::all();
+        return view('user.index', ['products' => $products]);
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,1 +1,34 @@
-商品一覧
+<x-app-layout>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        ホーム
+      </h2>
+  </x-slot>
+
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+          <div class="flex flex-wrap">
+            @foreach($products as $product)
+              <div class="w-1/4 p-2 md:p-4 mt-2">
+                {{-- <a href="{{ route('owner.products.edit', ['product' => $product->id]) }}"> --}}
+                  <div class="border rounded-md p-2 md:p-4">
+                    <div class="mb-4">
+                      <div>
+                        <x-thumbnail filename="{{ $product->imageFirst->filename ?? '' }}" type="products" />
+                        <div class="text-gray-700">
+                          {{ $product->name }}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            @endforeach
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</x-app-layout>
