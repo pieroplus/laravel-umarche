f5da52b48778e8464c74eb2955656eaf79a365ed
pieroplus <hey_taxi_stop_please@yahoo.co.jp>
2023-03-27 01:01:51 +0900

    表示順

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -11,25 +11,27 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:users');
+public function __construct()
+{
+    $this->middleware('auth:users');
 
-        $this->middleware(function ($request, $next) {
-            $id = $request->route()->parameter('item');
-            if (!is_null($id)) {
-                $itemId = Product::availableItems()->where('products.id', '=', $id)->exists();
-                if (!$itemId) {
-                    abort(404);
-                }
+    $this->middleware(function ($request, $next) {
+        $id = $request->route()->parameter('item');
+        if (!is_null($id)) {
+            $itemId = Product::availableItems()->where('products.id', '=', $id)->exists();
+            if (!$itemId) {
+                abort(404);
             }
-            return $next($request); 
-        });
-    }
+        }
+        return $next($request); 
+    });
+}
     
-    public function index(): View
+    public function index(Request $request): View
     {
-        $products = Product::availableItems()->get();
+        $products = Product::availableItems()
+            ->sortOrder($request->sort)
+            ->get();
         return view('user.index', ['products' => $products]);
     }
 
